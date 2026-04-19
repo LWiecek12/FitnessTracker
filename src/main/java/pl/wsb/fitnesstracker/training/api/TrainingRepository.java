536845894f023f47f2259f252d9bc08f06e2fc46
LWiecek12 <lwiecek12@gmail.com>
2026-04-19 18:40:16 +0200

    Lab03: Encje, Repozytoria i schemat bazy - poprawione4

--- src/main/java/pl/wsb/fitnesstracker/training/api/TrainingRepository.java
+++ src/main/java/pl/wsb/fitnesstracker/training/api/TrainingRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface TrainingRepository extends JpaRepository<Training, Long> {
 
-    // To jest zapytanie NATIVE SQL (na ocenę 5).
+
     // Odwołujemy się bezpośrednio do tabel z bazy danych ('trainings') i kolumn ('distance', 'user_id')
     @Query(
             value = "SELECT SUM(distance) FROM trainings WHERE user_id = :userId",
